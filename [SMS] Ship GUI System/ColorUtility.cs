3151da7b2ac74e2f13e49178ffc4b4627dc01562
MaXeN9818 <89645855+MaXeN9818@users.noreply.github.com>
2024-09-23 22:15:12 +0200

    [ADD][PROJECT] - Add ini management class(WIP) - Add Utility Color Class (WIP) - Project move

---
+++ [SMS] Ship GUI System/ColorUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRageMath;
+
+namespace IngameScript
+{
+    internal class ColorUtility
+    {
+        public bool TryGetColorFromString(string colorString, out Color color) // TODO: Ho cambiato idea meglio aggiungere all'inizio la dicitura di tipo es (RGBA o HEX)
+        {
+            char[] div = { ' ', ';', ',' };
+
+            string[] splitStringColor = colorString.Split(div);
+            if (splitStringColor.Length == 4)
+            {
+                try
+                {
+                    color = new Color()
+                    {
+                        R = byte.Parse(splitStringColor[0]),
+                        G = byte.Parse(splitStringColor[1]),
+                        B = byte.Parse(splitStringColor[2]),
+                        A = byte.Parse(splitStringColor[3]),
+                    };
+                    return true;
+                }
+                catch (Exception)
+                {
+                    color = new Color();
+                    return false;
+                }
+            }
+
+            color = new Color();
+            return false;
+
+        }
+    }
+}
---
+++ [SMS] Ship GUI System/Program.cs
@@ -0,0 +1,75 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+
+        public readonly string Title = "[SMS-GUI]";
+        public readonly string Version = "V1.0.0";
+
+        public ScreenLogger Logger { get; }
+        private MyCommandLine _commandLine = new MyCommandLine();
+        public MyIni PBConfigs { get; } = new MyIni();
+
+        public Program()
+        {
+            // Init Logs Screen
+            IMyTextSurfaceProvider logsLCD = GridTerminalSystem.GetBlockWithName("SMS Logs LCD") as IMyTextSurfaceProvider;
+            Logger = new ScreenLogger(this, "Logs", logsLCD, 0);
+#if DEBUG
+            Logger.Debug = true;
+#endif
+            Logger.LogInfo("Script Init Starting");
+
+
+            // Parse PB Custom Data
+            Logger.LogInfo("Loading configs");
+            MyIniParseResult iniParseRes;
+
+            if (string.IsNullOrEmpty(Me.CustomData))
+                Me.CustomData = ConfigINI.InitializeMyIniConfig();
+
+            if (!PBConfigs.TryParse(Me.CustomData, out iniParseRes))
+            {
+                Logger.LogCritical("Could not parse PB Custom Data");
+                throw new Exception("Could not parse PB Custom Data"); //TODO Gestire exception
+            }
+
+            ConfigINI.InitializeConfigSettings(PBConfigs);
+
+            string _errorMessage;
+            if(!ConfigINI.checkMyIniConfig(out _errorMessage)) 
+            {
+                Logger.LogCritical($"Error too configure MyINI Config: {_errorMessage}");
+                throw new Exception($"Error too configure MyINI Config: {_errorMessage}"); //TODO Gestire exception
+            }
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+        }
+    }
+}
