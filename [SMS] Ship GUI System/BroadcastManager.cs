53a759361547793c6ca2962a793b244522647fd1
MaXeN9818 <89645855+MaXeN9818@users.noreply.github.com>
2024-10-01 00:17:37 +0200

    [ADD] - Add Brodcast Message Listener {WIP}

---
+++ [SMS] Ship GUI System/BroadcastManager.cs
@@ -0,0 +1,55 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class BroadcastManager
+        {
+            private MyGridProgram _gridProgram;
+            private Program _program;
+            private IMyBroadcastListener _broadcastListener;
+            private Action<MyIGCMessage> _broadcastMessageHandlers;
+
+
+            public BroadcastManager(MyGridProgram myGridProgram) 
+            {
+                _gridProgram = myGridProgram;
+                _program = (Program)myGridProgram;
+                _program.Logger.LogInfo("Init Broadcast");
+
+            }
+
+            public bool SubcriberBrodcastMessage(string BroadcastTag, bool setCallback = true)
+            {
+                _broadcastListener = _gridProgram.IGC.RegisterBroadcastListener(BroadcastTag);
+                if (setCallback) _broadcastListener.SetMessageCallback("BROADCAST");
+                _program.Logger.LogInfo("Startup Broadcast Handler");
+                return true;
+            }
+
+
+                public IMyBroadcastListener GetBroadcastListener() => _broadcastListener;
+
+
+        }
+    }
+}
--- [SMS] Ship GUI System/Program.cs
+++ [SMS] Ship GUI System/Program.cs
@@ -14,13 +14,13 @@ namespace IngameScript
         public ScreenLogger Logger { get; }
         private MyCommandLine _commandLine = new MyCommandLine();
         public MyIni PBConfigs { get; } = new MyIni();
-
         public ConfigINI Configs = new ConfigINI();
+        public BroadcastManager broadcastManager;
 
         public Program()
         {
             // Init Logs Screen
-            IMyTextSurfaceProvider logsLCD = GridTerminalSystem.GetBlockWithName("SMS Logs LCD") as IMyTextSurfaceProvider;
+            IMyTextSurfaceProvider logsLCD = GridTerminalSystem.GetBlockWithName("SMS GUI Logs LCD") as IMyTextSurfaceProvider;
             Logger = new ScreenLogger(this, "Logs", logsLCD, 0);
 #if DEBUG
             Logger.Debug = true;
@@ -36,8 +36,7 @@ namespace IngameScript
 
             if (!PBConfigs.TryParse(Me.CustomData, out iniParseRes))
             {
-                Logger.LogCritical("Could not parse PB Custom Data");
-                throw new Exception("Could not parse PB Custom Data"); //TODO Gestire exception
+                Logger.LogFatal("Could not parse PB Custom Data");
             }
 
             Configs.InitializeConfigSettings(PBConfigs);
@@ -45,9 +44,13 @@ namespace IngameScript
             string _errorMessage;
             if (!Configs.checkMyIniConfig(out _errorMessage))
             {
-                Logger.LogCritical($"Error too configure MyINI Config: {_errorMessage}");
-                throw new Exception($"Error too configure MyINI Config: {_errorMessage}"); //TODO Gestire exception
+                Logger.LogFatal(_errorMessage);
             }
+
+
+            broadcastManager = new BroadcastManager(this);
+            broadcastManager.SubcriberBrodcastMessage(Configs.BroadcastTag);
+
         }
 
         public void Save()
@@ -56,6 +59,35 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
+            if ((updateSource & UpdateType.IGC) > 0)
+            {
+                Logger.LogDebug("IGC Trigger");
+                TestBroadcastHandler(broadcastManager.GetBroadcastListener().AcceptMessage());
+            }
         }
+
+
+        void TestBroadcastHandler(MyIGCMessage msg)
+        {
+            // NOTE: called on ALL received messages; not just 'our' tag
+
+            if (msg.Tag != Configs.BroadcastTag) 
+            {
+                Logger.LogWarning("Tag not valid");
+                return; // not our message
+
+            }
+                
+
+            if (msg.Data is string)
+            {
+                Logger.LogInfo("Received Test Message");
+                Logger.LogInfo(" Source=" + msg.Source.ToString("X"));
+                Logger.LogInfo(" Data=\"" + msg.Data + "\"");
+                Logger.LogInfo(" Tag=" + msg.Tag);
+            }
+        }
+
+
     }
 }
